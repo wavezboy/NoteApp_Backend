3b6fd5cf4a8220affdbb3a56aa6ddca939c999d5
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-05-27 07:45:17 +0100

    change cookie settings

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(
       maxAge: 60 * 60 * 1000,
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "strict",
     },
     rolling: true,
     store: MongoStore.create({
